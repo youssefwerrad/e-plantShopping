476bf140ca07d0fe0c8ceb5e5cfa626e8e2cccd3
youssef werrad <youssefwerrad@gmail.com>
2026-01-28 17:53:16 +0000

    Updated

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,9 +8,9 @@ import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const dispatch = useDispatch();
     const cartItems = useSelector(state => state.cart.items);
-    const [showCart, setShowCart] = useState(true);
+    const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(true);
-    const [showAboutUs, setShowAboutUs] = useState(true);
+    const [showAboutUs, setShowAboutUs] = useState(false);
     const [addedToCart, setAddedToCart] = useState({});
 
     // Update addedToCart state when cartItems change
